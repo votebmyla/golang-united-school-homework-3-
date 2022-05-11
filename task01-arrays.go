3dffa51dbab457eea82f2a990bc03fceaa989f12
votebmyla <votebmyla@gmail.com>
2022-05-11 15:20:18 +0500

    first task solution

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	for _, v := range input {
+		result = result + v
+	}
+	result = result / float32(len(input))
 	return
 }
